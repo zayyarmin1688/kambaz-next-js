e86475014675bf93173282dfee790972326e8809
Zay Yar Min <zayarminsam1688@gmail.com>
2025-12-01 20:10:06 -0500

    eslint disable shenanigans

--- app/(Kambaz)/Account/Users/page.tsx
+++ app/(Kambaz)/Account/Users/page.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // app/(Kambaz)/Account/Users/page.tsx
 "use client";
 
--- app/(Kambaz)/Courses/[cid]/People/Details.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Details.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 // app/(Kambaz)/Courses/[cid]/People/Details.tsx
 "use client";
 
